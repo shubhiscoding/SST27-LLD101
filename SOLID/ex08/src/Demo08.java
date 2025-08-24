60816c044454e4c8546062a458fc96a43634ce7a
shubhiscoding <mygitcoin@gmail.com>
2025-08-24 23:01:52 +0530

    8  done

--- SOLID/ex08/src/Demo08.java
+++ SOLID/ex08/src/Demo08.java
@@ -1,6 +1,9 @@
 public class Demo08 {
     public static void main(String[] args) {
-        Vehicle v = new Bicycle();
-        v.startEngine(); // crash
+        Bicycle v = new Bicycle();
+        v.pedal(5); // crash
+
+        Car c = new Car();
+        c.startEngine();
     }
 }
