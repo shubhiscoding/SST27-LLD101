271876cd85a81d14e05022dc0dbeda0cb2339e4c
shubhiscoding <mygitcoin@gmail.com>
2025-08-24 22:52:20 +0530

    6 down 4 more to go

--- SOLID/ex09/src/Demo09.java
+++ SOLID/ex09/src/Demo09.java
@@ -1,5 +1,6 @@
 public class Demo09 {
     public static void main(String[] args) {
-        new OrderController().create("ORD-1");
+        SqlOrderRepository db = new SqlOrderRepository();
+        new OrderController(db).create("ORD-1");
     }
 }
--- SOLID/ex09/src/OrderController.java
+++ SOLID/ex09/src/OrderController.java
@@ -1,7 +1,10 @@
 public class OrderController {
+    private DbQuery db;
+    OrderController(DbQuery db){
+        this.db = db;
+    }
     void create(String id){
-        SqlOrderRepository repo = new SqlOrderRepository(); // hard dependency
-        repo.save(id);
+        db.save(id);
         System.out.println("Created order: " + id);
     }
 }
