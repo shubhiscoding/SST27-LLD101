c86e1040617d46c9818480bcafc30953174986f0
shubhiscoding <mygitcoin@gmail.com>
2025-08-24 23:21:16 +0530

    fixed 10

--- SOLID/ex05/src/Demo05.java
+++ SOLID/ex05/src/Demo05.java
@@ -5,6 +5,6 @@ public class Demo05 {
     }
     public static void main(String[] args) {
         System.out.println(areaAfterResize(new Rectangle())); // 20
-        System.out.println(areaAfterResize(new Square()));    // 16 (!) violates expectation
+        // System.out.println(areaAfterResize(new Square()));    // 16 (!) violates expectation
     }
 }
--- SOLID/ex05/src/Square.java
+++ SOLID/ex05/src/Square.java
@@ -1,5 +1,6 @@
 
-public class Square extends Rectangle {
-    @Override void setWidth(int w){ this.w = this.h = w; }
-    @Override void setHeight(int h){ this.w = this.h = h; }
+public class Square {
+    int length;
+    
+    void setLenght(int w){ this.length = w; }
 }
