f91f399223ab040cff381071bbc4235d388a8b3d
shubhiscoding <mygitcoin@gmail.com>
2025-08-25 00:21:42 +0530

    Fing done

--- SOLID/ex05/src/Rectangle.java
+++ SOLID/ex05/src/Rectangle.java
@@ -1,6 +1,15 @@
-public class Rectangle {
+public class Rectangle extends Shape implements ResizeRectangle{
     protected int w,h;
     void setWidth(int w){ this.w = w; }
     void setHeight(int h){ this.h = h; }
-    int area(){ return w*h; }
+    @Override
+    int area() {
+        return this.w * this.h;
+    }
+    @Override
+    public int areaAfterResize(int l, int w) {
+        setHeight(l);
+        setWidth(w);
+        return this.area();
+    }
 }
--- SOLID/ex05/src/Square.java
+++ SOLID/ex05/src/Square.java
@@ -1,6 +1,17 @@
 
-public class Square {
+public class Square extends Shape implements ResizeSquare{
     int length;
     
     void setLenght(int w){ this.length = w; }
+
+    @Override
+    int area() {
+        return length * length;
+    }
+
+    @Override
+    public int areaAfterResize(int s) {
+        setLenght(s);
+        return this.area();
+    }
 }
